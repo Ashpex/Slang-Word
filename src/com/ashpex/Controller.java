4bc50d6259ef9a79324434f747392962e7eefe29
ashpex <ashpex@pm.me>
2022-03-20 09:29:35 +0700

    Add history class and function

--- src/com/ashpex/Controller.java
+++ src/com/ashpex/Controller.java
@@ -1,8 +1,16 @@
 package com.ashpex;
+import javax.swing.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Random;
 
 public class Controller{
     final String PATH = "slang.txt";
     final String DEFAULT_PATH = "default.txt";
     final String HISTORY_PATH = "history.dat";
-
+    private static final Entry<String, SlangWord> entry = null;
+    private HashMap<String, SlangWord> slang = new HashMap<>();
+    public History history = new History();
 }
---
+++ src/com/ashpex/History.java
@@ -0,0 +1,62 @@
+package com.ashpex;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class History {
+    private ArrayList<String> historyList = new ArrayList<String>();
+    public History () {
+        try{
+            File file = new File("history.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_16));
+            String line;
+            while ((line = br.readLine()) != null) {
+                historyList.add(line);
+                if (historyList.size() > 100) {
+                    historyList.remove(0);
+                }
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Add(String historyEntry) {
+        Date date = java.util.Calendar.getInstance().getTime();
+        String strDateFormat = "yyyy-MM-dd HH:mm:ss";
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat(strDateFormat);
+        String formattedDate = sdf.format(date);
+        historyList.add(formattedDate + " " + historyEntry);
+
+    }
+
+    public String showHistory() {
+        StringBuilder sb = new StringBuilder();
+        for (String line : historyList) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    public void saveHistory(){
+        try{
+            File file = new File("history.txt");
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_16));
+            for (String line : historyList) {
+                bw.write(line);
+                bw.newLine();
+            }
+            bw.flush();
+            bw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
